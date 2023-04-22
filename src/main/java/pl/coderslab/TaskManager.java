3d833160ea940a7995c6c348aa7c62c09e42dd14
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-04-22 21:35:53 +0200

    update methods

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,4 +1,35 @@
 package pl.coderslab;
+import java.nio.*;
+import java.util.*;
 
 public class TaskManager {
+
+    public static void main(String[] args){
+
+    }
+
+    // wczytywanie danych z pliku
+    public static void readData(){
+
+    }
+    // zapisywanie danych do pliku
+    public static void saveData(){
+
+    }
+    // wyświetlanie zadań
+    public static void showTasks(){
+
+    }
+    // dodawanie zadania
+    public static void addTask(){
+
+    }
+    // usuwanie zadania ze sprawdzeniem poprawności numeru zadania do usunięcia
+    public static void removeTask(){
+
+    }
+
 }
+
+
+
